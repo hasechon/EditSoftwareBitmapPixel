028217b6acd25468e2d4f47693406d136674e0fe
hasechon <drivevolleyer@gmail.com>
2023-07-12 06:13:08 +0900

    first commit

---
+++ set_answer/MainPage.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Media;
+
+namespace winrt::set_answer::implementation
+{
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+
+        SoftwareBitmap softwareBitmap = GetSoftwareBitmap(200, 200);
+        SetImageSourceFromSoftwareBitmap(softwareBitmap, Img1());
+    }
+
+    SoftwareBitmap MainPage::GetSoftwareBitmap(int nWidth, int nHeight)
+    {
+        SoftwareBitmap softwareBitmap(BitmapPixelFormat::Bgra8, nWidth, nHeight, BitmapAlphaMode::Premultiplied);
+        BitmapBuffer buffer = softwareBitmap.LockBuffer(BitmapBufferAccessMode::Write);
+        uint8_t* pixels = buffer.CreateReference().data();
+        int nPixelsSize = nWidth * nHeight * 4;
+        for (int i = 0; i < nPixelsSize; i += 4)
+        {
+            pixels[i] = 255;
+            pixels[i + 1] = 0;
+            pixels[i + 2] = 0;
+            pixels[i + 3] = 255; // add
+        }
+        return softwareBitmap;
+    }
+
+    Windows::Foundation::IAsyncAction MainPage::SetImageSourceFromSoftwareBitmap(SoftwareBitmap softwareBitmap, winrt::Windows::UI::Xaml::Controls::Image image)
+    {
+        Imaging::SoftwareBitmapSource bitmapSource;
+        co_await bitmapSource.SetBitmapAsync(softwareBitmap);
+        image.Source(bitmapSource);
+    }
+}
---
+++ set_answer/MainPage.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+using namespace winrt::Windows::Graphics::Imaging;
+
+namespace winrt::set_answer::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage() {};
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+        SoftwareBitmap GetSoftwareBitmap(int nWidth, int nHeight);
+        Windows::Foundation::IAsyncAction SetImageSourceFromSoftwareBitmap(SoftwareBitmap softwareBitmap, winrt::Windows::UI::Xaml::Controls::Image image);
+    };
+}
+
+namespace winrt::set_answer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
